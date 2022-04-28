b615b9bfc3a4f1f4653e9c1bac015992fe08677d
Ahmed Elsayed <ahmedelsayed3157@gmail.com>
2022-04-28 06:04:24 +0200

    make header icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightBulbIcon,
+  SearchCircleIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <header>
-      <h2>hello this is a header </h2>
-
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Home" Icon={LightBulbIcon} />
+        <HeaderItem title="Home" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Home" Icon={CollectionIcon} />
+        <HeaderItem title="Home" Icon={SearchCircleIcon} />
+        <HeaderItem title="Home" Icon={UserIcon} />
+      </div>
       <Image
         src="https://links.papareact.com/ua6"
         className="object-contain"
