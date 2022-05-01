08ce7399e185ae126ece31e8026ad37f816f9405
Ahmed Elsayed <ahmedelsayed3157@gmail.com>
2022-05-01 23:48:42 +0200

    fix some issues for deploy

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
-import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = ({ result }) => {
   const base_url = "https://image.tmdb.org/t/p/original";
   return (
-    <div
-      ref={ref}
-      className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
-    >
+    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -31,6 +27,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-});
+};
 
 export default Thumbnail;
